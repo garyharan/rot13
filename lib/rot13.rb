26ca51a87b8f56a91ff8bb8aeffae93c268bb179
Gary Haran <gary.haran@gmail.com>
2013-05-30 09:56:09 -0400

    do not translate in place

--- lib/rot13.rb
+++ lib/rot13.rb
@@ -3,6 +3,6 @@ require "i18n"
 
 module Rot13
   def self.cipher(str)
-    I18n.transliterate(str).tr!("A-Za-z", "N-ZA-Mn-za-m")
+    I18n.transliterate(str).tr("A-Za-z", "N-ZA-Mn-za-m")
   end
 end
